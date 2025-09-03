489fced8c6224464103e477853635a95a166205e
Samistus <40127394+Samistus@users.noreply.github.com>
2025-09-03 18:42:18 +0300

    Todo 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
+const url = "http://localhost:3001"
 
 function App() {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setTasks(response.data)
+    })
+    .catch(error => {
+    alert(error.response.data ? error.response.data.message : error)
+    })
+    },[])
 
   const addTask = () => {
-    setTasks([...tasks,task])
-      setTask('');
-    }
+    const newTask = {description: task}
+
+    axios.post(url + "/create", {task: newTask})
+    .then(response => {
+      setTasks([...tasks, response.data])
+      setTask("")
+    })
+    .catch(error => {
+      alert(error.response ? error.response.data.error.message : error)
+    })
+  }
 
   const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter(item => item !== deleted)
-    setTasks(withoutRemoved)
+    axios.delete(url + "/delete/" + deleted)
+    .then(response => {
+      setTasks(tasks.filter(item => item.id !==deleted))
+    })
+    .catch(error => {
+      alert(error.response ? error.response.data.error.message : error)
+    })
   }
 
   return (
@@ -24,7 +51,7 @@ function App() {
         onChange={e => setTask(e.target.value)}
         onKeyDown={e => {
           if(e.key === 'Enter'){
-            e.preventDefault();
+            e.preventDefault()
             addTask()
           }
         }}
@@ -33,11 +60,11 @@ function App() {
       <ul>
         {
           tasks.map(item =>(
-            <li>
-              {item}
+            <li key ={item.id}>
+              {item.description}
             <button
             className='delete-button'
-            onClick={() => deleteTask(item)}>
+            onClick={() => deleteTask(item.id)}>
             Delete
             </button>
               </li>
